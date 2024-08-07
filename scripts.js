3002e661450cec8646d914bbe47e5ccaff7222b0
zoe <zoe.flumini@gmail.com>
2024-08-07 21:33:33 +0200

    framework

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function gotologIn() {
+    window.location.href = 'logIn.html';
+}
+function gotobout() {
+    window.location.href = 'about.html';
+}
+function gotoregister() {
+    window.location.href = 'register.html';
+}
+
+////////////// Home button for all Subpages ////////////////
+function gotoindex() {
+    window.location.href = 'index.html';
+}
